3e013ccd1519b324289beaf5e16ee235b38e4e50
antollare <54421200+antollare@users.noreply.github.com>
2021-04-18 17:47:44 -0400

    Changed RestaurantFragment to Account
    I refactored this class in order to better reflect what it is

---
+++ AccountFragment.java
@@ -0,0 +1,77 @@
+package com.example.lettuceeat;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AccountFragment extends Fragment {
+    AccountListener mListener;
+    Button buttonUpdateAccount, buttonSettings, buttonLogout;
+
+    public AccountFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_restaurants, container, false);
+
+        buttonUpdateAccount = view.findViewById(R.id.buttonUpdateAccount);
+        buttonSettings = view.findViewById(R.id.buttonSettings);
+        buttonLogout = view.findViewById(R.id.buttonLogout);
+        getActivity().setTitle("Account");
+
+        buttonUpdateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.gotoUpdate();
+            }
+        });
+
+        buttonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.gotoSettings();
+            }
+        });
+
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.logout();
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        try {
+            mListener = (AccountListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement RegisterListener");
+        }
+    }
+
+    interface AccountListener{
+        void gotoUpdate();
+        void gotoSettings();
+        void logout();
+    }
+}
